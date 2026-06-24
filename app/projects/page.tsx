d3949e2885b17968a144e7099d0b8a318257f28f
lanryweezy <lanryweezy@gmail.com>
2026-06-24 22:08:19 +0100

    fix: remove local SVG function definitions from projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -460,14 +460,6 @@ export default function ProjectsPage() {
   )
 }
 
-function FolderOpen(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-      <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
-    </svg>
-  )
-}
-
 function Megaphone(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
